be6d8e1e7d031b7bfca2cf42e09ccd5ef0868b47
jiongjiongStrive <982567656@qq.com>
2023-04-21 08:58:15 +0800

    repair arxiv dataset

--- train_gnn_arxiv.py
+++ train_gnn_arxiv.py
@@ -1,30 +1,90 @@
+import argparse
+
 import torch
 import torch.nn.functional as F
+
 import torch_geometric.transforms as T
+from torch_geometric.nn import GCNConv, SAGEConv
+
 from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
-from util import train_decorator
+
 from util.logger import Logger
-from models import GAT, GCN, SAGE, QW, QG, C
-from util.global_variable import *
-import numpy as np
 
 
-def train(model, data, train_idx, optimizer, train_dec: train_decorator.TrainDecorator, cur_epoch):
-    train_dec.create_bash_command(cur_epoch, args.bl_weight, args.bl_activation)
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
+                 dropout):
+        super(GCN, self).__init__()
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(GCNConv(in_channels, hidden_channels, cached=True))
+        self.bns = torch.nn.ModuleList()
+        self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(
+                GCNConv(hidden_channels, hidden_channels, cached=True))
+            self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
+        self.convs.append(GCNConv(hidden_channels, out_channels, cached=True))
+
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    def forward(self, x, adj_t):
+        for i, conv in enumerate(self.convs[:-1]):
+            x = conv(x, adj_t)
+            x = self.bns[i](x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, adj_t)
+        return x.log_softmax(dim=-1)
+
+
+class SAGE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers,
+                 dropout):
+        super(SAGE, self).__init__()
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConv(in_channels, hidden_channels))
+        self.bns = torch.nn.ModuleList()
+        self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(SAGEConv(hidden_channels, hidden_channels))
+            self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
+        self.convs.append(SAGEConv(hidden_channels, out_channels))
+
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    def forward(self, x, adj_t):
+        for i, conv in enumerate(self.convs[:-1]):
+            x = conv(x, adj_t)
+            x = self.bns[i](x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, adj_t)
+        return x.log_softmax(dim=-1)
+
+
+def train(model, data, train_idx, optimizer):
     model.train()
-    # 量化权重
-    train_dec.quantify_weight(model, 0, cur_epoch)
-    # 绑定钩子函数，记录各层的输入
-    train_dec.bind_hooks(model, cur_epoch)
+
     optimizer.zero_grad()
     out = model(data.x, data.adj_t)[train_idx]
     loss = F.nll_loss(out, data.y.squeeze(1)[train_idx])
     loss.backward()
-    # 量化梯度
-    train_dec.quantify_activation(model)
     optimizer.step()
-    # 清除钩子
-    train_dec.clear_hooks(model, 0, cur_epoch)
+
     return loss.item()
 
 
@@ -52,13 +112,19 @@ def test(model, data, split_idx, evaluator):
 
 
 def main():
+    parser = argparse.ArgumentParser(description='OGBN-Arxiv (GNN)')
+    parser.add_argument('--device', type=int, default=0)
+    parser.add_argument('--log_steps', type=int, default=1)
+    parser.add_argument('--use_sage', action='store_true')
+    parser.add_argument('--num_layers', type=int, default=3)
+    parser.add_argument('--hidden_channels', type=int, default=256)
+    parser.add_argument('--dropout', type=float, default=0.5)
+    parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--runs', type=int, default=10)
+    args = parser.parse_args()
     print(args)
-    # 定义量化权重和梯度的lambda函数以及权重clip函数
-    weight_quantification = lambda x, scale: QW(x, args.bl_weight, scale)
-    grad_quantiication = lambda x: QG(x, args.bl_grad, args.bl_rand, args.lr)
-    grad_clip = lambda x: C(x, args.bl_weight)
 
-    train_dec = train_decorator.TrainDecorator(weight_quantification, grad_quantiication, grad_clip, run_recorder)
     device = f'cuda:{args.device}' if torch.cuda.is_available() else 'cpu'
     device = torch.device(device)
 
@@ -66,9 +132,6 @@ def main():
                                      transform=T.ToSparseTensor())
 
     data = dataset[0]
-    adj_matrix = data.adj_t.set_diag().to_dense().numpy()
-    run_recorder.record('', 'adj_matrix.csv', adj_matrix, delimiter=',', fmt='%d')
-    activity = np.sum(adj_matrix.astype(np.float), axis=None) / np.size(adj_matrix)
     data.adj_t = data.adj_t.to_symmetric()
     data = data.to(device)
 
@@ -79,13 +142,8 @@ def main():
         model = SAGE(data.num_features, args.hidden_channels,
                      dataset.num_classes, args.num_layers,
                      args.dropout).to(device)
-    elif args.use_gcn:
-        model = GCN(data.num_features, args.hidden_channels,
-                    dataset.num_classes, args.num_layers,
-                    args.dropout, bl_weight=args.bl_weight, bl_activate=args.bl_activate, bl_error=args.bl_error,
-                    recorder=run_recorder, adj_activity=activity).to(device)
     else:
-        model = GAT(data.num_features, args.hidden_channels,
+        model = GCN(data.num_features, args.hidden_channels,
                     dataset.num_classes, args.num_layers,
                     args.dropout).to(device)
 
@@ -96,7 +154,7 @@ def main():
         model.reset_parameters()
         optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
         for epoch in range(1, 1 + args.epochs):
-            loss = train(model, data, train_idx, optimizer, train_dec, epoch)
+            loss = train(model, data, train_idx, optimizer)
             result = test(model, data, split_idx, evaluator)
             logger.add_result(run, result)
 
@@ -110,8 +168,6 @@ def main():
                       f'Test: {100 * test_acc:.2f}%')
 
         logger.print_statistics(run)
-        model.record_cost()
-
     logger.print_statistics()
 
 
