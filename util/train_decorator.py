ea5f060f989fed0c10aabb9314fdca628d2cad19
jiongjiongStrive <982567656@qq.com>
2023-04-29 09:33:39 +0800

    debug

--- util/train_decorator.py
+++ util/train_decorator.py
@@ -31,6 +31,7 @@ class TrainDecorator:
                                          delimiter=',', fmt='%10.5f')
                 param.data = self.weight_quantification(model.weight_acc[name], model.weight_scale[name]).to(
                     next(model.parameters()).device)
+                print('weight q:', model.weight_acc[name])
                 # if self.recorder is not None and batch_index == 0:
                 #     self.recorder.record(f'layer_run/epoch{cur_epoch}', f'{name}_before.csv',
                 #                          param.data.T.to('cpu').data.numpy(),
@@ -53,7 +54,9 @@ class TrainDecorator:
                 param.grad.data = self.grad_quantification(param.grad.data).data
                 # print('after:', param.grad.data)
                 # 裁剪，限制权重范围
+                print('grad q bc:', model.weight_acc[name])
                 w_acc = self.wage_grad_clip(model.weight_acc[name]).to(next(model.parameters()).device)
+                print('grad q ac:', model.weight_acc[name])
                 w_acc -= param.grad.data
                 model.weight_acc[name] = w_acc
 
@@ -65,6 +68,7 @@ class TrainDecorator:
                 handle.remove()
             for name, param in list(model.named_parameters())[::-1]:
                 if 'weight' in name and self.recorder is not None and 'convs' in name:
+                    print('clear hook:', model.weight_acc[name])
                     self.recorder.record(f'layer_run/epoch{cur_epoch}', f'{name}_after.csv',
                                          param.data.T.to('cpu').data.numpy(),
                                          delimiter=',', fmt='%10.5f')
